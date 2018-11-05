124c68bf62f11b2c7cbbac202040098238d7b247
Ankur Pathak <ankurpathak@live.in>
2018-11-05 11:53:58 +0530

    mockito multiple throw tested

---
+++ src/test/java/com/ankurpathak/github/MockitoThrow.java
@@ -0,0 +1,49 @@
+package com.ankurpathak.github;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("unchecked")
+public class MockitoThrow {
+
+    @Mock
+    private List<String> list;
+
+    @Test
+    public void testMockitoThrowMultipleFirstStyle1(){
+        when(list.size()).thenThrow(RuntimeException.class).thenThrow(NumberFormatException.class);
+        assertThrows(RuntimeException.class, list::size);
+        assertThrows(NumberFormatException.class, list::size);
+    }
+
+    @Test
+    public void testMockitoThrowMultipleSecondStyle1(){
+        doThrow(RuntimeException.class).doThrow(NumberFormatException.class).when(list).size();
+        assertThrows(RuntimeException.class, list::size);
+        assertThrows(NumberFormatException.class, list::size);
+    }
+
+    @Test
+    public void testMockitoThrowMultipleSecondStyle2(){
+        doThrow(RuntimeException.class, NumberFormatException.class).when(list).size();
+        assertThrows(RuntimeException.class, list::size);
+        assertThrows(NumberFormatException.class, list::size);
+    }
+
+    @Test
+    public void testMockitoThrowMultipleFirstStyle2(){
+        when(list.size()).thenThrow(RuntimeException.class, NumberFormatException.class);
+        assertThrows(RuntimeException.class, list::size);
+        assertThrows(NumberFormatException.class, list::size);
+    }
+}
